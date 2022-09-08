46a437cb4cb55715959166f30362cd495a0c33d5
Parker Parham <parkerparham@gmail.com>
2022-09-08 15:38:43 -0400

    commit tree 0d1ffe28ace33dadb8149aa0d614ec43584a79f8

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  const [list, setList] = useState(TASKS);
+
+  function addTask(taskObj){
+   const newList = [...list, taskObj]
+   setList(newList)
+  }
+  function handleRemove(text){
+    const newList = list.filter((task) => task.text !== text);
+    setList(newList)
+  }
+
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter  categories={CATEGORIES} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
+      <NewTaskForm addTask={addTask} categories={CATEGORIES} />
+      <TaskList handleRemove={handleRemove} list={list} setList={setList} tasks={list} selectedCategory={selectedCategory} />
     </div>
   );
 }
